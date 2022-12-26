4aba194540c126e6d961f3e1233cf3b1e2c28b8d
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-12-26 21:34:04 +0900

    renamed

--- baekjoon/A+B.py
+++ baekjoon/A+B.py
@@ -1,7 +1,2 @@
-import sys
-
-a = int(sys.stdin.readline())
-
-for i in range(a):
-    A, B = map(int, sys.stdin.readline().split())
-    print(A + B)
+A, B = input().split()
+print(int(A) + int(B))
